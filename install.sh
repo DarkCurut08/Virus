57fd19eeab2351a8e7240a5d96486ce96fd3b19d
DarkCurut08 <42291700+DarkCurut08@users.noreply.github.com>
2018-11-22 17:37:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,16 +6,20 @@ d="\033[37;1m"
 e="\033[36;1m"
 f="\033[39;1m"
 echo $a
+chmod +x Moreno77.sh
 cd /storage/emulated/0/
 mkdir Virus
 cd /data/data/com.termux/files/home/
-chmod +x Moreno77.sh
+echo $a
 pkg install cowsay
 clear
+echo $b
 pkg install python2
 clear
+echo $c
 pip2 install lolcat
 clear
+echo $d
 pkg install figlet
 clear
 figlet 'Curut BlackHat' | lolcat
@@ -28,6 +32,7 @@ echo " [!] Loading....." | lolcat
 sleep 0.4
 clear
 figlet 'Curut BlackHat' | lolcat
+echo $f
 pkg install wget
 sleep 0.4
 clear
