7c0afbc0e3a953619ecec9ec75923008bd6de7b8
DarkCurut08 <42291700+DarkCurut08@users.noreply.github.com>
2018-11-22 16:56:34 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,77 @@
+a="\033[32;1m"
+b="\033[33;1m"
+b="\033[34;1m"
+c="\033[35;1m"
+d="\033[37;1m"
+e="\033[36;1m"
+f="\033[39;1m"
+echo $a
+cd /storage/emulated/0/
+mkdir Virus
+cd /data/data/com.termux/files/home/
+chmod +x Moreno77.sh
+clear
+figlet 'Curut BlackHat' | lolcat
+sleep 0.4
+clear
+figlet 'DarkCurut08' | lolcat
+sleep 0.4
+clear
+echo " [!] Loading....." | lolcat
+sleep 0.4
+clear
+figlet 'Curut BlackHat' | lolcat
+sleep 0.4
+clear
+figlet 'DarkCurut08' | lolcat
+sleep 0.4
+clear
+echo " [!] Loading....." | lolcat
+sleep 0.4
+clear
+figlet 'Curut BlackHat' | lolcat
+sleep 0.4
+clear
+figlet 'DarkCurut08' | lolcat
+sleep 0.4
+clear
+echo " [!] Loading....." | lolcat
+sleep 0.4
+clear
+figlet 'Curut BlackHat' | lolcat
+sleep 0.4
+clear
+figlet 'DarkCurut08' | lolcat
+sleep 0.4
+clear
+echo " [!] Loading....." | lolcat
+sleep 0.4
+clear
+figlet 'Curut BlackHat' | lolcat
+sleep 0.4
+clear
+figlet 'DarkCurut08' | lolcat
+sleep 0.4
+clear
+echo " [!] Loading....." | lolcat
+sleep 0.4
+clear
+figlet 'Curut BlackHat' | lolcat
+sleep 0.4
+clear
+figlet 'DarkCurut08' | lolcat
+sleep 0.4
+clear
+echo " [!] Loading....." | lolcat
+sleep 0.4
+clear
+figlet 'Curut BlackHat' | lolcat
+sleep 0.4
+clear
+figlet 'DarkCurut08' | lolcat
+sleep 0.4
+clear
+echo " [!] Loading....." | lolcat
+sleep 0.4
+clear
+echo " [!] ketik sh Moreno77.sh Untuk Menjalankan Toolsnya" | lolcat
